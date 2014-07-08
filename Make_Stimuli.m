8d6490db1848b33bf54aad89450ae8043788d57b
RagnaViola <R.Soennecken@gmail.com>
2014-07-08 15:04:13 +0100

    Update Make_Stimuli.m

--- Make_Stimuli.m
+++ Make_Stimuli.m
@@ -1,11 +1,13 @@
-% Top-level script for generating stimuli.
+function [ stimulus_1, stimulus_2, stimulus_3, stimulus_4, stimulus_5 ] = Make_Stimuli(  )
+%top_level function to generate the stimuli
+
 
 orientations = linspace(0,175,36);
-frequencies = linspace(0, 20, 36);
+frequencies = linspace(1, 20, 36);
 
 %first stimulus
-o_index = round(rand(1)*36);
-f_index = round(rand(1)*36);
+o_index = randi([1,36],1,1);
+f_index = randi([1,36],1,1);
 
 o_index_old = 0;
 f_index_old = 0;
@@ -13,16 +15,20 @@ f_index_old = 0;
 o_index = index_finding(o_index, o_index_old);
 f_index = index_finding(f_index, f_index_old);
 
-o_1=orientations(o_index)
-f_1=frequencies(f_index)
+o_1=orientations(o_index);
+f_1=frequencies(f_index);
 
 figure(1)
 gaborIm = gaborFn(400, f_1, 50, o_1, 0, .005);
 imagesc(gaborIm, [-1 1]);
-axis image; axis off; colormap gray(1024);
+axis image; axis off; colormap gray(256);
 set(gca,'pos', [0 0 1 1]);
 set(gcf, 'menu', 'none', 'Color',[.5 .5 .5]);
-saveas(gcf,'Stimulus_1','png')
+%saveas(gcf,'Stimulus_1','png')
+%imshow(gaborIm,gcf)
+stimulus_1=gaborIm;
+%imshow(stimulus_1);
+
 %return
 
 
@@ -30,22 +36,23 @@ saveas(gcf,'Stimulus_1','png')
 o_index_old = o_index;
 f_index_old = f_index;
 
-o_index = round(rand(1)*36);
-f_index = round(rand(1)*36);
+o_index = randi([1,36],1,1);
+f_index = randi([1,36],1,1);
 
 o_index = index_finding(o_index, o_index_old);
 f_index = index_finding(f_index, f_index_old);
 
-o_2=orientations(o_index)
-f_2=frequencies(f_index)
+o_2=orientations(o_index);
+f_2=frequencies(f_index);
 
 figure(2)
 gaborIm = gaborFn(400, f_2, 50, o_2, 0, .005);
 imagesc(gaborIm, [-1 1]);
-axis image; axis off; colormap gray(1024);
+axis image; axis off; colormap gray(256);
 set(gca,'pos', [0 0 1 1]);
 set(gcf, 'menu', 'none', 'Color',[.5 .5 .5]);
-saveas(gcf, 'Stimulus_2', 'png')
+%saveas(gcf, 'Stimulus_2', 'png')
+stimulus_2=gaborIm;
 %return
 
 
@@ -53,22 +60,23 @@ saveas(gcf, 'Stimulus_2', 'png')
 o_index_old = o_index;
 f_index_old = f_index;
 
-o_index = round(rand(1)*36);
-f_index = round(rand(1)*36);
+o_index = randi([1,36],1,1);
+f_index = randi([1,36],1,1);
 
 o_index = index_finding(o_index, o_index_old);
 f_index = index_finding(f_index, f_index_old);
 
-o_3=orientations(o_index)
-f_3=frequencies(f_index)
+o_3=orientations(o_index);
+f_3=frequencies(f_index);
 
 figure(3)
 gaborIm = gaborFn(400, f_3, 50, o_3, 0, .005);
 imagesc(gaborIm, [-1 1]);
-axis image; axis off; colormap gray(1024);
+axis image; axis off; colormap gray(256);
 set(gca,'pos', [0 0 1 1]);
 set(gcf, 'menu', 'none', 'Color',[.5 .5 .5]);
-saveas(gcf, 'Stimulus_3', 'png')
+%saveas(gcf, 'Stimulus_3', 'png')
+stimulus_3=gaborIm;
 %return
 
 
@@ -76,43 +84,46 @@ saveas(gcf, 'Stimulus_3', 'png')
 o_index_old = o_index;
 f_index_old = f_index;
 
-o_index = round(rand(1)*36);
-f_index = round(rand(1)*36);
+o_index = randi([1,36],1,1);
+f_index = randi([1,36],1,1);
 
 o_index = index_finding(o_index, o_index_old);
 f_index = index_finding(f_index, f_index_old);
 
-o_4=orientations(o_index)
-f_4=frequencies(f_index)
+o_4=orientations(o_index);
+f_4=frequencies(f_index);
 
 figure(4)
 gaborIm = gaborFn(400, f_4, 50, o_4, 0, .005);
 h=imagesc(gaborIm, [-1 1]);
-axis image; axis off; colormap gray(1024);
+axis image; axis off; colormap gray(256);
 set(gca,'pos', [0 0 1 1]);
 set(gcf, 'menu', 'none', 'Color',[.5 .5 .5]);
-saveas(gcf,'Stimulus_4','png')
+%saveas(gcf,'Stimulus_4','png')
+stimulus_4=gaborIm;
 %return
 
 %fifth stimulus
 o_index_old = o_index;
 f_index_old = f_index;
 
-o_index = round(rand(1)*36);
-f_index = round(rand(1)*36);
+o_index = randi([1,36],1,1);
+f_index = randi([1,36],1,1);
 
 o_index = index_finding(o_index, o_index_old);
 f_index = index_finding(f_index, f_index_old);
 
-o_5=orientations(o_index)
-f_5=frequencies(f_index)
+o_5=orientations(o_index);
+f_5=frequencies(f_index);
 
 figure(5)
 gaborIm = gaborFn(400, f_5, 50, o_5, 0, .005);
 imagesc(gaborIm, [-1 1]);
-axis image; axis off; colormap gray(1024);
+axis image; axis off; colormap gray(256);
 set(gca,'pos', [0 0 1 1]);
 set(gcf, 'menu', 'none', 'Color',[.5 .5 .5]);
-saveas(gcf,'Stimulus_5','png')
-return
+%saveas(gcf,'Stimulus_5','png')
+stimulus_5=gaborIm;
+%return
+end
 
